bbfab754d5ee1d38d438941a46d5f940a905055e
unknown <salmaiihab@gmail.com>
2021-09-11 12:33:02 +0200

    production without minification

--- webpack.prod.js
+++ webpack.prod.js
@@ -29,6 +29,7 @@ module.exports ={
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
+            minify: 'false'
         }),
     ]
 }
