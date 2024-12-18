245a5e391f680dc1beb2a2fb1510658cc6efc50d
salmaehab <67350115+salmaehab@users.noreply.github.com>
2024-12-18 11:10:12 +0200

    test

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -23,4 +23,62 @@ fetch("http://localhost:8082/add", {
                 } 
             }
 
+            if (Client.is_url(URL))
+                {
+                 console.log("form submitted")
+                 document.getElementById("agreement").innerHTML = "Please wait, Data is being Fetched"
+             fetch("http://localhost:8082/add", {
+                                     "method": "POST",
+                                     mode: 'cors',
+                                     "credentials": "same-origin",
+                                     headers: {"Content-Type": "application/json"},
+                                     body:  JSON.stringify({url: URL})
+                                     }).then(res=> res.json())
+                                     .then(data=>{
+                                      document.getElementById("agreement").innerHTML =`agreement: ${data.analyzedData.agreement}`
+                                      document.getElementById("confidence").innerHTML =`confidence: ${data.analyzedData.confidence}`
+                                      document.getElementById("subjectivity").innerHTML =`subjectivity: ${data.analyzedData.subjectivity}`
+                                      document.getElementById("irony").innerHTML =`irony: ${data.analyzedData.irony}`
+                                     }) 
+                                     .catch(error=>console.log(error))
+                             } 
+                         } if (Client.is_url(URL))
+                            {
+                             console.log("form submitted")
+                             document.getElementById("agreement").innerHTML = "Please wait, Data is being Fetched"
+                         fetch("http://localhost:8082/add", {
+                                                 "method": "POST",
+                                                 mode: 'cors',
+                                                 "credentials": "same-origin",
+                                                 headers: {"Content-Type": "application/json"},
+                                                 body:  JSON.stringify({url: URL})
+                                                 }).then(res=> res.json())
+                                                 .then(data=>{
+                                                  document.getElementById("agreement").innerHTML =`agreement: ${data.analyzedData.agreement}`
+                                                  document.getElementById("confidence").innerHTML =`confidence: ${data.analyzedData.confidence}`
+                                                  document.getElementById("subjectivity").innerHTML =`subjectivity: ${data.analyzedData.subjectivity}`
+                                                  document.getElementById("irony").innerHTML =`irony: ${data.analyzedData.irony}`
+                                                 }) 
+                                                 .catch(error=>console.log(error))
+                                         } 
+                                     } if (Client.is_url(URL))
+                                        {
+                                         console.log("form submitted")
+                                         document.getElementById("agreement").innerHTML = "Please wait, Data is being Fetched"
+                                     fetch("http://localhost:8082/add", {
+                                                             "method": "POST",
+                                                             mode: 'cors',
+                                                             "credentials": "same-origin",
+                                                             headers: {"Content-Type": "application/json"},
+                                                             body:  JSON.stringify({url: URL})
+                                                             }).then(res=> res.json())
+                                                             .then(data=>{
+                                                              document.getElementById("agreement").innerHTML =`agreement: ${data.analyzedData.agreement}`
+                                                              document.getElementById("confidence").innerHTML =`confidence: ${data.analyzedData.confidence}`
+                                                              document.getElementById("subjectivity").innerHTML =`subjectivity: ${data.analyzedData.subjectivity}`
+                                                              document.getElementById("irony").innerHTML =`irony: ${data.analyzedData.irony}`
+                                                             }) 
+                                                             .catch(error=>console.log(error))
+                                                     } 
+                                                 }
 export{handleSubmit}
